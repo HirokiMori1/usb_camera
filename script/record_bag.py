afdd1ffb33df3d5fc31e81026523c88ad5ea4c30
HirokiMori <h.mori@kufusha.com>
2020-07-07 16:42:04 +0900

    add  record_bag

---
+++ script/record_bag.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+import rosbag
+from sensor_msgs.msg import Image
+
+class Camera():
+    def __init__(self):
+        self.image_subscriber = rospy.Subscriber("/image_raw/compressed", Image, self.image_callback)
+        self.bag = rosbag.Bag('test.bag', 'w')
+
+    def image_callback(self, data):
+        self.bag.write("/image_raw/compressed", data)
+
+if __name__ == "__main__":
+    rospy.init_node("record_camera")
+
+    camera = Camera()
+
+    rate = rospy.Rate(50)
+    while not rospy.is_shutdown():
+        rate.sleep()
+    
+    camera.bag.close()
+    rospy.spin()
